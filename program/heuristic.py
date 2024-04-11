26880e561bed67ff88c0b5738b72c0a0b6029a90
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2024-04-10 21:05:08 -0300

    refactor: minor changes

--- program/heuristic.py
+++ program/heuristic.py
@@ -1,7 +1,21 @@
+'''
+Lines 11 to 18: 
+Copied from: https://stackoverflow.com/questions/3838329/how-can-i-check-if-two-segments-intersect
+'''
 from typing import List
 from entities.city import City
 from external import *
 from math import sqrt
+
+
+# Determines if three points are listed in a counterclockwise order
+def ccw(A, B, C):
+    return (C.y-A.y) * (B.x-A.x) > (B.y-A.y) * (C.x-A.x)
+
+
+# Return true if line segments AB and CD intersect
+def intersect(A, B, C, D):
+    return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
 
 
 def distance_between_cities (first: City, second: City):
--- program/main.py
+++ program/main.py
@@ -1,8 +1,23 @@
-from heuristic import *
-from draw import *
+'''
+Lines 16-29 and 49-50: 
+Copied and modified from: https://chat.openai.com/share/2b7858a6-1524-4892-aadf-7ae3e650aa60
+
+Line 32:
+Copied from: https://stackoverflow.com/questions/110362/how-can-i-find-the-current-os-in-python
+'''
 from entities.city import City
-from external import get_numeric
+from draw import *
+from heuristic import *
 import os
+import platform
+import re
+
+
+# Read numberic values from instances files
+def get_numeric(list):
+    primitive = [value.strip() for value in list if re.match(r'^\d+$', value.strip())]
+    numerical = [int(value) for value in primitive]
+    return numerical
 
 
 script_directory = os.path.dirname(os.path.abspath(__file__))
@@ -15,8 +30,13 @@ for filepath in os.listdir(instances_directory):
       print("-" * 100)
       print(f"Lendo arquivo: {filename}")
 
+      operational_system = platform.platform().lower()
       # get last "/" section: file name, i.e "mTSP-n12-m1"
-      primitive = filename.split("/")[-1]
+      if 'windows' in operational_system:
+        primitive = filename.split("\\")[-1]
+      else:
+        primitive = filename.split("/")[-1]
+         
       n_primitive, m_primitive = primitive.split("-")[1], primitive.split("-")[2] # i.e, n12 and m1
       n = int(n_primitive[1:]) + 1 # n12 has 13 cities. 12 is the last city's index
       m = int(m_primitive[1:])
