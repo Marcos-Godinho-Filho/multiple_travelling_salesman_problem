048665f69a0299c7b0c4eee8515668fb7aff03bc
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2024-05-18 09:34:48 -0300

    feat: add genetic algorithm code

--- program/meta_heuristics/genetic_algorithm.py
+++ program/meta_heuristics/genetic_algorithm.py
@@ -2,98 +2,124 @@
 # https://medium.com/aimonks/traveling-salesman-problem-tsp-using-genetic-algorithm-fea640713758
 # https://towardsdatascience.com/introduction-to-optimization-with-genetic-algorithm-2f5001d9964b
 # https://towardsdatascience.com/genetic-algorithm-implementation-in-python-5ab67bb124a6
+'''
+Theorical Reference: 
+    * https://medium.com/@Data_Aficionado_1083/genetic-algorithms-optimizing-success-through-evolutionary-computing-f4e7d452084f
 
-""" 
-pseudo-code:
+Code Reference:
+    * https://github.com/Rayan-Ali1083/Genetic-Algorithm
+'''
+import random
 
-def run_ga(n_population, n_generations,
-           crossover_per, mutation_per):
-    
-    population = initial_population(n_population)
-    fitness_probs = fitness_prob(population)
-    
-    parents_list = []
-    for i in range(0, int(crossover_per * n_population)):
-        parents_list.append(roulette_wheel(population,
-                                           fitness_probs))
+POPULATION_SIZE = 500
+MUTATION_RATE = 0.1
+TARGET = '123456790123456'
+GENES = ' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.,!0123456789'
 
-    offspring_list = []    
-    for i in range(0,len(parents_list), 2):
-        offspring_1, offspring_2 = crossover(parents_list[i], 
-                                             parents_list[i+1])
+# Initilize population
+def initialize_population(target):
+    population = list()
+    target_len = len(target) # chromossome size => number of genes
+    for i in range(POPULATION_SIZE):
+        chromossome = list()
+        for j in range(target_len):
+            chromossome.append(random.choice(GENES))
+        population.append(chromossome)
+    return population
+    # population = [[random.choice(GENES) for j in range(target_len)] for i range(POPULATION_SIZE)]
 
-        mutate_threashold = random.random()
-        if(mutate_threashold > (1-mutation_per)):
-            offspring_1 = mutation(offspring_1)
 
-        mutate_threashold = random.random()
-        if(mutate_threashold > (1-mutation_per)):
-            offspring_2 = mutation(offspring_2)
+def calculate_fitness(target, chromossome_from_population):
+    difference = 0
+    for target_position, gene in zip(target, chromossome_from_population):
+        if target_position != gene:
+            difference += 1
+    return [chromossome_from_population, difference]
 
-        offspring_list.append(offspring_1)
-        offspring_list.append(offspring_2)
 
-    mixed_offspring = parents_list + offspring_list
+def select_from_population(population):
+    # Sort population according to fitness
+    sorted_population = sorted(population, key = lambda chromossome: chromossome[1])
 
-    fitness_probs = fitness_prob(mixed_offspring)
-    sorted_fitness_indices = np.argsort(fitness_probs)[::-1]
-    best_fitness_indices = sorted_fitness_indices[0:n_population]
-    best_mixed_offsrping = []
-    for i in best_fitness_indices:
-        best_mixed_offsrping.append(mixed_offspring[i])
+    # Return top 50% of population => Most adapted
+    return sorted_population[:int(0.5 * POPULATION_SIZE)]
 
-    for i in range(0, n_generations):
-        
-        fitness_probs = fitness_prob(best_mixed_offsrping)
-        parents_list = []
-        for i in range(0, int(crossover_per * n_population)):
-            parents_list.append(roulette_wheel(best_mixed_offsrping, 
-                                               fitness_probs))
 
-        offspring_list = []    
-        for i in range(0,len(parents_list), 2):
-            offspring_1, offspring_2 = crossover(parents_list[i], 
-                                                 parents_list[i+1])
+def crossover(selected_population, chromossome_len, population):
+    offspring = list()
 
-            mutate_threashold = random.random()
-            if(mutate_threashold > (1-mutation_per)):
-                offspring_1 = mutation(offspring_1)
+    for i in range(POPULATION_SIZE):
+        # One parent from the top 50%
+        parent1 = random.choice(selected_population)[0] # [0] gets chromossome it self, without fitness
+        # The other one is random, can be from bottom 50% for example
+        parent2 = random.choice(population[:int(POPULATION_SIZE * 50)])[0]
 
-            mutate_threashold = random.random()
-            if(mutate_threashold > (1-mutation_per)):
-                offspring_2 = mutation(offspring_2)
+        # Crossover point will also be random
+        crossover_point = random.randint(1, chromossome_len - 1)
+        child = parent1[:crossover_point] + parent2[crossover_point:]
+        offspring.append(child)
 
-            offspring_list.append(offspring_1)
-            offspring_list.append(offspring_2)
+    return offspring
 
-        mixed_offspring = parents_list + offspring_list
-        fitness_probs = fitness_prob(mixed_offspring)
-        sorted_fitness_indices = np.argsort(fitness_probs)[::-1]
-        best_fitness_indices = sorted_fitness_indices[0:int(0.8*n_population)]
 
-        best_mixed_offsrping = []
-        for i in best_fitness_indices:
-            best_mixed_offsrping.append(mixed_offspring[i])
-            
-        old_population_indices = [random.randint(0, (n_population - 1)) 
-                                  for j in range(int(0.2*n_population))]
-        for i in old_population_indices:
-            best_mixed_offsrping.append(population[i])
-            
-        random.shuffle(best_mixed_offsrping)
-            
-    return best_mixed_offsrping
-    
-best_mixed_offsrping = run_ga(cities_names, n_population,
-                              n_generations, crossover_per, mutation_per)
+def mutate(offspring, mutation_rate):
+    mutated_offspring = list()
 
-total_dist_all_individuals = []
-for i in range(0, n_population):
-    total_dist_all_individuals.append(total_dist_individual(best_mixed_offsrping[i]))
+    for chromossome in offspring:
+        for gene in range(len(chromossome)):
+            if random.random() < mutation_rate:
+                chromossome[gene] = random.choice(GENES)
+        mutated_offspring.append(chromossome)
 
-index_minimum = np.argmin(total_dist_all_individuals)
-minimum_distance = min(total_dist_all_individuals)
+    return mutated_offspring
 
-shortest_path = best_mixed_offsrping[index_minimum]
 
-"""
+def replace(new_generation, population):
+    for i in range(len(population)):
+        # If current fitness is greater than new fitness, it is bad and we must replace it
+        if population[i][1] > new_generation[i][1]:
+            population[i][0] = new_generation[i][0]
+            population[i][1] = new_generation[i][1]
+    return population
+
+
+def main(population_size, mutation_rate, target, genes):
+    # 1) Initialize population
+    initial_population = initialize_population(target)
+    population = list()
+    target_found = False
+    generation = 1
+
+    # 2) Order population according to fitness
+    for chromossome in initial_population:
+        population.append(calculate_fitness(target, chromossome))
+
+    # 3) Natural selection
+    while not target_found:
+        # 3.1) Select 50% best chromossomes from population
+        selected = select_from_population(population)
+
+        # 3.2) Perform crossover between selected population and the rest of it
+        population = sorted(population, key = lambda chromossome: chromossome[1])
+        crossovered = crossover(selected, len(target), population)
+
+        # 3.3) Perform mutation
+        mutated = mutate(crossovered, mutation_rate)
+
+        new_generation = [calculate_fitness(target, chromossome) for chromossome in mutated]
+
+        # 3.4) Natural selection itself
+        population = replace(new_generation, population)
+
+        # If the best chromossome in population has a fitness of 0, it has found the target
+        if (population[0][1] == 0):
+            print('Target found')
+            print('String: ' + str(population[0][0]) + ' Generation: ' + str(generation) + ' Fitness: ' + str(population[0][1]))
+            target_found = True
+
+        print('String: ' + str(population[0][0]) + ' Generation: ' + str(generation) + ' Fitness: ' + str(population[0][1]))
+        generation += 1
+
+
+if __name__ == '__main__':
+    main(POPULATION_SIZE, MUTATION_RATE, TARGET, GENES)
